f66d844344ad3493261fec44a0de3bb82b414a8a
ggambopce <ggambopce@gmail.com>
2025-04-07 06:33:36 +0900

    ✨ JwtAuthenticationFilter 생성

--- board-back/src/main/java/com/jino/board_back/global/security/provider/JwtProvider.java
+++ board-back/src/main/java/com/jino/board_back/global/security/provider/JwtProvider.java
@@ -6,15 +6,42 @@ import java.time.temporal.ChronoUnit;
 
 import org.springframework.stereotype.Component;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
 @Component
 public class JwtProvider {
 
     private String secretKey = "SecretK3y";
 
-    // JWT 생성
+    // JWT 생성메서드
     public String create(String email) {
 
         Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
 
+        String jwt = Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .setSubject(email).setIssuedAt(new Date()).setExpiration(expiredDate)
+                .compact();
+
+        return jwt;
+    }
+
+    // JWT 검증, JWT에서 정보 추출 메서드
+    public String validate(String jwt) {
+
+        Claims claims = null;
+
+        try {
+            claims = Jwts.parser().setSigningKey(secretKey)
+                    .parseClaimsJws(jwt).getBody();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+
+        return claims.getSubject();
+    }
 
 }
