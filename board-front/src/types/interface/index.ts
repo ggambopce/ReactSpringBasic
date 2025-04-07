6af4dbca53979960fc9c3f77dd9aec56c5f3bd96
ggambopce <ggambopce@gmail.com>
2025-04-08 06:43:40 +0900

    ✨ 공통 객체 타입 선언

---
+++ board-front/src/types/interface/index.ts
@@ -0,0 +1,10 @@
+import FavoriteListItem from "./favorite-list-item.interface";
+import BoardListItem from "./board-list-item.interface";
+import CommentListItem from "./comment-list-item.interface";
+
+export type {
+    FavoriteListItem,
+    BoardListItem,
+    CommentListItem,
+
+} 
