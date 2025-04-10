ec30c5838fab54d7bf2af101a79170775b2258db
ggambopce <ggambopce@gmail.com>
2025-04-10 09:06:28 +0900

    ✨ CommentItem Mock

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
-import { top3BoardListMock } from 'mocks';
+import { commentListMock, top3BoardListMock } from 'mocks';
 import Top3Item from 'components/top3Item';
 import CommentItem from 'components/commentItem';
 
 function App() {
   return (
     <>
-    <div></div>
-      <CommentItem />
+    <div style={{padding: '0 20px', display: 'flex', flexDirection: 'column', gap: '30px'}}>
+      {commentListMock.map(commentListItem => <CommentItem commentListItem={commentListItem}/>)}
+    </div>
+      
     </>
   );
 }
--- board-front/src/mocks/index.ts
+++ board-front/src/mocks/index.ts
@@ -1,7 +1,9 @@
 import latesBoardListMock from './latest-board-list-mock';
 import top3BoardListMock from './top-3-board-list-mock';
+import commentListMock from './comment-list-mock';
 
 export {
     latesBoardListMock,
-    top3BoardListMock
+    top3BoardListMock,
+    commentListMock,
 }
