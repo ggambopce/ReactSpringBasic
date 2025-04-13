b45602a06ceb1ba86159de9df92fe9b2a654543a
ggambopce <ggambopce@gmail.com>
2025-04-14 08:24:43 +0900

    ✨ Header 레이아웃

--- board-front/src/layouts/header/index.tsx
+++ board-front/src/layouts/header/index.tsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import './style.css'
+import { useNavigate } from 'react-router-dom'
+import { MAIN_PATH } from 'constant';
 
 
 //          component: Header 레이아웃          //
 export default function Header() {
   
+    //          function: 네비게이트 함수           //
+    const navigator = useNavigate();
+
+    //          event handler: 로고 클릭 이벤트 처리 함수           //
+    const onLogoClickHandler = () => {
+        navigator(MAIN_PATH());
+    }
     //          render: Header 레이아웃 렌더링          //
     return (
-    <div>Header</div>
+    <div id='header'>
+        <div className='header-container'>
+            <div className='header-left-box' onClick={onLogoClickHandler}>
+                <div className='icon-box'>
+                    <div className='icon logo-edu-icon'></div>
+                </div>
+                <div className='header-logo'>{'JINOs Board'}</div>
+            </div>
+            <div className='header-right-box'></div>
+        </div>
+    </div>
   )
 }
 
+ 
