d134f53a0db10c91f1171930eaacda03ea086583
ggambopce <ggambopce@gmail.com>
2025-04-17 08:59:54 +0900

    ✨ 로그인 service

--- board-back/src/main/java/com/jino/board_back/domain/auth/service/AuthService.java
+++ board-back/src/main/java/com/jino/board_back/domain/auth/service/AuthService.java
@@ -2,11 +2,15 @@ package com.jino.board_back.domain.auth.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.jino.board_back.domain.auth.dto.request.SignInRequestDto;
 import com.jino.board_back.domain.auth.dto.request.SignUpRequestDto;
 import com.jino.board_back.domain.auth.dto.response.SignUpResponseDto;
+import com.jino.board_back.domain.auth.dto.response.SignInResponseDto;
 
 public interface AuthService {
 
     ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto);
+
+    ResponseEntity<? super SignInResponseDto> signIn(SignInRequestDto dto);
 
 }
--- board-back/src/main/java/com/jino/board_back/domain/auth/service/implement/AuthSecrviceImplement.java
+++ board-back/src/main/java/com/jino/board_back/domain/auth/service/implement/AuthSecrviceImplement.java
@@ -5,12 +5,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import com.jino.board_back.domain.auth.dto.request.SignInRequestDto;
 import com.jino.board_back.domain.auth.dto.request.SignUpRequestDto;
+import com.jino.board_back.domain.auth.dto.response.SignInResponseDto;
 import com.jino.board_back.domain.auth.dto.response.SignUpResponseDto;
 import com.jino.board_back.domain.auth.service.AuthService;
 import com.jino.board_back.domain.user.entity.UserEntity;
 import com.jino.board_back.domain.user.repository.UserRepository;
 import com.jino.board_back.global.dto.response.ResponseDto;
+import com.jino.board_back.global.security.provider.JwtProvider;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthSecrviceImplement implements AuthService {
 
     private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
 
     private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
@@ -54,6 +58,34 @@ public class AuthSecrviceImplement implements AuthService {
         }
 
         return SignUpResponseDto.success();
+    }
+
+    @Override
+    public ResponseEntity<? super SignInResponseDto> signIn(SignInRequestDto dto) {
+
+        String token = null;
+
+        try {
+
+            String email = dto.getEmail();
+            UserEntity userEntity = userRepository.findByEmail(email);
+            if (userEntity == null)
+                return SignInResponseDto.signInFail();
+
+            String password = dto.getPassword();
+            String encodedPassword = userEntity.getPassword();
+            boolean isMatched = passwordEncoder.matches(password, encodedPassword);
+            if (!isMatched)
+                return SignInResponseDto.signInFail();
+
+            token = jwtProvider.create(email);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return ResponseDto.databaseError();
+        }
+
+        return SignInResponseDto.success(token);
     }
 
 }
