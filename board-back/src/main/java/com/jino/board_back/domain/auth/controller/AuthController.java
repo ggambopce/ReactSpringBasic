5ae5d614fda25685691f8506ba156569880a3594
ggambopce <ggambopce@gmail.com>
2025-04-17 05:29:34 +0900

    ✨ Auth Controller

---
+++ board-back/src/main/java/com/jino/board_back/domain/auth/controller/AuthController.java
@@ -0,0 +1,31 @@
+package com.jino.board_back.domain.auth.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jino.board_back.domain.auth.dto.request.SignUpRequestDto;
+import com.jino.board_back.domain.auth.dto.response.SignUpResponseDto;
+import com.jino.board_back.domain.auth.service.AuthService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<? super SignUpResponseDto> signUp(@RequestBody @Valid SignUpRequestDto requestBody) {
+
+        ResponseEntity<? super SignUpResponseDto> response = authService.signUp(requestBody);
+        return response;
+    }
+
+}
