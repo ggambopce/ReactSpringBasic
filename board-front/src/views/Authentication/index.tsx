bff10450b056a2d0e6e6c17a3ccbb0b9d3a4afec
ggambopce <ggambopce@gmail.com>
2025-04-18 09:05:14 +0900

    ✨ Error Box 조건

--- board-front/src/views/Authentication/index.tsx
+++ board-front/src/views/Authentication/index.tsx
@@ -72,10 +72,13 @@ export default function Authentication() {
               <InputBox ref={passwordRef} label='패스워드' type={passwordType} placeholder='비밀번호를 입력해주세요' error={error} value={password} setValue={setPassword} icon={passwordButtonIcon} onButtonClick={onPasswordButtonClickHandler} onKeyDown={onPasswordKeyDownHandler}/>
             </div>
             <div className='auth-card-bottom'>
-              <div className='auth-sign-in-error-box'>
-                <div className='auth-sign-in-error-message'>
-                  {'이메일 주소 또는 비밀번호를 잘못 입력했습니다. \n입력하신 내용을 다시 확인해주세요.'}</div>
-              </div>
+              {error && 
+                 <div className='auth-sign-in-error-box'>
+                   <div className='auth-sign-in-error-message'>
+                   {'이메일 주소 또는 비밀번호를 잘못 입력했습니다. \n입력하신 내용을 다시 확인해주세요.'}
+                   </div>
+               </div>
+              }
               <div className='black-large-full-button' onClick={onSignInButtonClickHandler}>{'로그인'}</div>
               <div className='auth-description-box'>
                 <div className='auth-description'>{'신규 사용자이신가요?'} <span className='auth-description-link' onClick={onSignUpButtonClickHandler}>{'회원 가입'}</span></div>
