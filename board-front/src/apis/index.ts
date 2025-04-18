03edd068fedb8e1ac3fa7a4f403d3601bdecbf95
ggambopce <ggambopce@gmail.com>
2025-04-18 10:21:51 +0900

    ✨ 로그인API  axios

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -1,4 +1,7 @@
+import axios from "axios";
 import { SignInRequestDto, SignUpRequestDto } from "./request/auth";
+import { SignInResponseDto } from "./response/auth";
+import { ResponseDto } from "./response";
 
 const DOMAIN = 'http://localhost:8080';
 
@@ -7,10 +10,20 @@ const API_DOMAIN = `${DOMAIN}/api/v1`;
 const SIGN_IN_URL = () => `${API_DOMAIN}/auth/sign-in`;
 const SIGN_UP_URL = () => `${API_DOMAIN}/auth/sign-up`;
 
-export const SignInRequest = (requestBody: SignInRequestDto) => {
-
+export const SignInRequest = async (requestBody: SignInRequestDto) => {
+    const result = await axios.post(SIGN_IN_URL(), requestBody)
+        .then(response => {
+            const responseBody: SignInResponseDto = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            if (!error.response.data) return null;
+            const responseBody: ResponseDto = error.response.data;
+            return responseBody;
+        })
+    return result;
 }
 
-export const SignUpRequest = (requestBody: SignUpRequestDto) => {
+export const SignUpRequest = async (requestBody: SignUpRequestDto) => {
     
 }
