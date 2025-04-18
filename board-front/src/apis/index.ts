d13dec693e33dbaf5d295b1cc596263bbdd3c32a
ggambopce <ggambopce@gmail.com>
2025-04-18 09:21:29 +0900

    ✨ Auth API

---
+++ board-front/src/apis/index.ts
@@ -0,0 +1,16 @@
+import { SignInRequestDto, SignUpRequestDto } from "./request/auth";
+
+const DOMAIN = 'http://localhost:8080';
+
+const API_DOMAIN = `${DOMAIN}/api/v1`;
+
+const SIGN_IN_URL = () => `${API_DOMAIN}/auth/sign-in`;
+const SIGN_UP_URL = () => `${API_DOMAIN}/auth/sign-up`;
+
+export const SignInRequest = (requestBody: SignInRequestDto) => {
+
+}
+
+export const SignUpRequest = (requestBody: SignUpRequestDto) => {
+    
+}
