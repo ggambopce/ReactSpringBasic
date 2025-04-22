77d198c855b9b62ea9a4d7e8a02ca868aa3a2ad7
ggambopce <ggambopce@gmail.com>
2025-04-22 09:34:13 +0900

    ✨ 게시물 이미지 서비스 로직

--- board-back/src/main/java/com/jino/board_back/domain/board/service/implement/BoardServiceImplement.java
+++ board-back/src/main/java/com/jino/board_back/domain/board/service/implement/BoardServiceImplement.java
@@ -1,5 +1,8 @@
 package com.jino.board_back.domain.board.service.implement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +11,8 @@ import com.jino.board_back.domain.board.dto.response.PostBoardResponseDto;
 import com.jino.board_back.domain.board.entity.BoardEntity;
 import com.jino.board_back.domain.board.repository.BoardRepository;
 import com.jino.board_back.domain.board.service.BoardService;
+import com.jino.board_back.domain.image.entity.ImageEntity;
+import com.jino.board_back.domain.image.repository.ImageRepository;
 import com.jino.board_back.domain.user.repository.UserRepository;
 import com.jino.board_back.global.dto.response.ResponseDto;
 
@@ -19,6 +24,7 @@ public class BoardServiceImplement implements BoardService {
 
     private final UserRepository userRepository;
     private final BoardRepository boardRepository;
+    private final ImageRepository imageRepository;
 
     @Override
     public ResponseEntity<? super PostBoardResponseDto> postBoard(PostBoardRequestDto dto, String email) {
@@ -30,6 +36,17 @@ public class BoardServiceImplement implements BoardService {
 
             BoardEntity boardEntity = new BoardEntity(dto, email);
             boardRepository.save(boardEntity);
+
+            int boardNumber = boardEntity.getBoardNumber();
+
+            List<String> boardImageList = dto.getBoardImageList();
+            List<ImageEntity> imageEntities = new ArrayList<>();
+
+            for (String image : boardImageList) {
+                ImageEntity imageEntity = new ImageEntity(boardNumber, image);
+                imageEntities.add(imageEntity);
+            }
+            imageRepository.saveAll(imageEntities);
 
         } catch (Exception exception) {
             exception.printStackTrace();
