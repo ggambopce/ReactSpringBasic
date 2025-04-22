ff8b4520988ba7a5a251f4561659fd1603a0ccfe
ggambopce <ggambopce@gmail.com>
2025-04-23 06:25:14 +0900

    ✨ 게시물 작성 레이아웃

--- board-front/src/views/Board/Write/index.tsx
+++ board-front/src/views/Board/Write/index.tsx
@@ -6,6 +6,30 @@ export default function BoardWrite() {
   
   //          render: 게시물 작성 화면 컴포넌트 렌더링           //
   return (
-    <div>게시물 작성 화면</div>
+    <div id='board-write-wrapper'>
+      <div className='board-write-container'>
+        <div className='board-write-box'>
+          <div className='board-write-title-box'>
+            <input className='board-write-title-input' />
+          </div>
+          <div className='divider'></div>
+          <div className='board-write-content-box'>
+            <textarea className='board-write-content-box' />
+            <div className='icon-button'>
+              <div className='icon image-box-light-icon'></div>
+            </div>
+            <input />
+          </div>
+          <div className='board-write-images-box'>
+            <div className='board-write-image-box'>
+              <img className='board-write-image'/>
+              <div className='icon-button image-close'>
+                <div className='icon close-icon'></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
