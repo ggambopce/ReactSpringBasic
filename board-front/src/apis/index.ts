496301eb85410e7b8ca0250131136412212d0b04
ggambopce <ggambopce@gmail.com>
2025-04-29 07:19:22 +0900

    ✨ 게시물 작성 api

--- board-front/src/apis/index.ts
+++ board-front/src/apis/index.ts
@@ -57,3 +57,21 @@ export const getSignInUserRequest = async (accessToken: string) => {
         });
     return result;
 }
+
+const FILE_DOMAIN = `${DOMAIN}/file`;
+
+const FILE_UPLOAD_URL = () => `${FILE_DOMAIN}/upload`;
+
+const multipartFormData = { headers: { 'Content-Type': 'multipart/form-data' } };
+
+export const fileUploadRequest =  async (data: FormData) => {
+    const result = await axios.post(FILE_UPLOAD_URL(), data, multipartFormData)
+        .then(response => {
+            const responseBody: string = response.data;
+            return responseBody;
+        })
+        .catch(error => {
+            return null;
+        })
+    return result;
+}
