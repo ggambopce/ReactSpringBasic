c2c200e41bf241585cda568fb5ed0c9a572aa2de
ggambopce <ggambopce@gmail.com>
2025-04-30 08:27:38 +0900

    ✨ 좋아요리스트 ResultSet

--- board-back/src/main/java/com/jino/board_back/domain/favorite/repository/FavoriteRepository.java
+++ board-back/src/main/java/com/jino/board_back/domain/favorite/repository/FavoriteRepository.java
@@ -1,13 +1,27 @@
 package com.jino.board_back.domain.favorite.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jino.board_back.domain.favorite.entity.FavoriteEntity;
 import com.jino.board_back.domain.favorite.entity.primaryKey.FavoritePK;
+import com.jino.board_back.domain.favorite.repository.resultSet.GetFavoriteResultSet;
 
 @Repository
 public interface FavoriteRepository extends JpaRepository<FavoriteEntity, FavoritePK> {
 
     FavoriteEntity findByBoardNumberAndUserEmail(Integer boardNumber, String userEmail);
+
+    @Query(value = "SELECT " +
+            "U.email AS email, " +
+            "U.nickname AS nickname, " +
+            "U.profile_image AS profileImage " +
+            "FROM favorite AS F " +
+            "INNER JOIN user AS U " +
+            "ON F.user_email = U.email " +
+            "WHERE F.board_number = ?1", nativeQuery = true)
+    List<GetFavoriteResultSet> getFavoriteResultSets(Integer boardNumber);
 }
---
+++ board-back/src/main/java/com/jino/board_back/domain/favorite/repository/resultSet/GetFavoriteResultSet.java
@@ -0,0 +1,9 @@
+package com.jino.board_back.domain.favorite.repository.resultSet;
+
+public interface GetFavoriteResultSet {
+    String getEmail();
+
+    String getNickname();
+
+    String getProfileImage();
+}
