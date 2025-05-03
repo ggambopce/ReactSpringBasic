22f51a289697bc961ca72273cf7f5d36fde62c2f
ggambopce <ggambopce@gmail.com>
2025-05-03 09:15:05 +0900

    ✨ 상세 페이지 상단 스타일

--- board-front/src/views/Board/Detail/index.tsx
+++ board-front/src/views/Board/Detail/index.tsx
@@ -4,6 +4,8 @@ import FavoriteItem from 'components/favoriteItem'
 import { CommentListItemType, FavoriteListItemType } from 'types/interface'
 import { commentListMock, favoriteListMock } from 'mocks'
 import CommentItem from 'components/commentItem'
+import Pagination from 'components/pagination'
+import defaultProfileImage from 'assets/image/default-profile-image.png';
 
 
 //          component: 게시물 상세 화면 컴포넌트           //
@@ -12,6 +14,14 @@ export default function BoardDetail() {
   //          component: 게시물 상세 상단 컴포넌트           //
   const BoardDedtailTop = () => {
     
+    //          state: more 버튼 상태          //
+    const [showMore, setShowMore] = useState<boolean>(false);
+
+    //          event handler: more 버튼 클릭 이벤트 처리          //
+    const onMoreButtonClickHandler = () => {
+      setShowMore(!showMore);
+    }
+
     //          render: 게시물 상세 상단 컴포넌트 렌더링           //
     return (
       <div id='board-detail-top'>
@@ -19,25 +29,27 @@ export default function BoardDetail() {
           <div className='board-detail-title'>{'제네릭(Generic)은 자바와 같은 언어에서 데이터 타입을 일반화할 수 있는 방법을 제공하는 기능이다'}</div>
           <div className='board-detail-top-sub-box'>
             <div className='board-detail-write-info-box'>
-              <div className='board-detail-writer-profile-image'></div>
+              <div className='board-detail-writer-profile-image' style={{backgroundImage: `url(${defaultProfileImage})`}}></div>
               <div className='board-detail-writer-nickname'>{'안녕나는지노'}</div>
               <div className='board-detail-info-divider'>{'\|'}</div>
               <div className='board-detail-write-date'>{'2025. 05. 03.'}</div>
             </div>
-            <div className='icon-button'>
+            <div className='icon-button' onClick={onMoreButtonClickHandler}>
               <div className='icon more-icon'></div>
             </div>
+            {showMore &&
             <div className='board-detail-more-box'>
               <div className='board-detail-update-button'>{'수정'}</div>
               <div className='divider'></div>
               <div className='board-detail-delete-button'>{'삭제'}</div>
             </div>
+            }
           </div>
         </div>
         <div className='divider'></div>
         <div className='board-detail-top-main'>
           <div className='board-detail-main-text'>{'제네릭을 사용하면 클래스, 인터페이스, 메서드가 다양한 데이터 타입을 처리할 수 있도록 코드의 유연성을 높일 수 있다. 이는 코드의 재사용성을 증가시키고, 컴파일 타임에서 타입 안전성을 보장할 수 있게 해준다.'}</div>
-          <div className='board-detail-main-image'></div>
+          <img className='board-detail-main-image' src='https://blog.kakaocdn.net/dn/nx9MF/btrvr9Mhfvz/N4mwWJEONQxcpsNnsJX6b1/img.jpg' />
         </div>
       </div>
     )
@@ -94,7 +106,7 @@ export default function BoardDetail() {
           </div>
           <div className='divider'></div>
           <div className='board-detail-bottom-comment-pagination-box'>
-            <
+            <Pagination />
           </div>
           <div className='board-detail-bottom-comment-input-box'>
             <div className='board-detail-comment-input-container'>
