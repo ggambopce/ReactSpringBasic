0bc9db1c88ffe6dc93e5f30ddea9489df7faf4ff
ggambopce <ggambopce@gmail.com>
2025-05-07 19:41:32 +0900

    🎨 페이지네이션 레이아웃

--- board-front/src/components/pagination/index.tsx
+++ board-front/src/components/pagination/index.tsx
@@ -6,6 +6,25 @@ export default function pagination() {
   
     //          render: 페이지네이션 컴포넌트 랜더링           //
     return (
-        <div></div>
+        <div id='paginaion-wrapper'>
+          <div className='pagination-change-link-box'>
+            <div className='icon-box-small'>
+              <div className='icon expand-left-icon'></div>
+            </div>
+            <div className='pagination-change-link-text'>{'이전'}</div>
+          </div>
+          <div className='pagination-divider'>{'|'}</div>
+
+          <div className='pagination-text-active'>{1}</div>
+          <div className='pagination-text'>{2}</div>
+
+          <div className='pagination-divider'>{'|'}</div>
+          <div className='pagination-change-link-box'>
+            <div className='pagination-change-link-text'>{'다음'}</div>
+            <div className='icon-box-small'>
+              <div className='icon expand-right-icon'></div>
+            </div>
+          </div>
+        </div>
   )
 }
